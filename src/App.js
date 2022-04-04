da362bdf27d854ff1ee696fa89ac7d8af2cc1fdc
amelie-forgeard <amelieforgeard@gmail.com>
2022-04-04 17:14:00 +0200

    title + 2buttonsMUI-2columns

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ import Header from './components/Header';
 
 function App() {
   return (
-<<<<<<< HEAD
     <ThemeProvider theme={theme}>
       <div className="App">
         <Header />
       </div>
     </ThemeProvider>
-=======
-    <div className="app">
-      <Header />
-    </div>
->>>>>>> fb5b1e66bd4bd54f9964024825e5be42c7f7743b
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,32 @@
-import { Container } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
+import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import logo from '../../assets/images/logo.jpg';
 
 export default function Header() {
   return (
-<<<<<<< HEAD
-    <Box>
-      
-=======
->>>>>>> fb5b1e66bd4bd54f9964024825e5be42c7f7743b
+    <Box sx={{
+      width: '100%',
+      maxWidth: 450,
+      p: 2,
+      border: '1px dashed grey',
+      display: 'flex',
+      justifyContent: 'space-between',
+    }}
+    >
+      <Typography
+        variant="h1"
+        component="div"
+        gutterBottom
+        sx={{
+          my: 'auto',
+          fontSize: 20,
+        }}
+        color="secondary"
+
+      >
+        C du prop's
+      </Typography>
       <ButtonGroup
         size="small"
         variant="contained"
@@ -20,21 +35,23 @@ export default function Header() {
         <Button
           color="secondary"
           size="small"
+          href="#"
+          sx={{ mt: 1 }}
+          onClick={() => console.log('inscription')}
         >
           S'inscrire
         </Button>
         <Button
           color="secondary"
           size="small"
+          href="#"
+          sx={{ mt: 1 }}
+          onClick={() => console.log('connexion')}
         >
           Se connecter
         </Button>
       </ButtonGroup>
-<<<<<<< HEAD
     </Box>
-=======
->>>>>>> fb5b1e66bd4bd54f9964024825e5be42c7f7743b
-
 
   );
 }
