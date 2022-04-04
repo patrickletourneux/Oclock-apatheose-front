3be6a2b57f9231b17e08e10bde6669b0785a1206
Webme <guillaume.gackel@gmail.com>
2022-04-04 14:35:04 +0200

    theming init

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,5 +1,50 @@
 import { createTheme } from '@mui/material';
 
-export default createTheme({
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00b8d4',
+    },
+    secondary: {
+      main: '#8bc34a',
+    },
+  },
 
+  typography: {
+    fontFamily: ['Roboto'].join(','),
+
+    h1: {
+      fontSize: 14,
+      fontWeight: 800,
+    },
+    h2: {
+      fontSize: 12,
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: 10,
+      fontWeight: 500,
+    },
+
+    body1: {
+      fontSize: 10,
+      fontWeight: 500,
+    },
+
+    button: {
+      fontSize: '1.5rem',
+    },
+  },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 800,
+      lg: 1024,
+      xl: 1200,
+    },
+  },
 });
+
+export default theme;
