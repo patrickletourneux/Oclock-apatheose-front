16ed7988b8357096af643180c797c6bd24c004d2
S-Axel <sabotieraxel@gmail.com>
2022-04-05 14:06:44 +0200

    set a working api url Add a signin route in api.js

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // TODO update url
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://54.37.154.200:10000/api/v1/',
 });
 
-export default api;
+export const signin = async ({ email, password }, setData, setError) => {
+  try {
+    const response = await api.post('signin', {
+      email,
+      password,
+    });
+    setData(response.data);
+  } catch (error) {
+    if (error.status === 400)
+      setError(error.data);
+    else
+      setError('Une erreur est survenue, veuillez réessayer plus tard');
+  }
+};
