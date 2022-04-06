901687c57520db51bfe4db1a3b7314b5f56f7a49
S-Axel <sabotieraxel@gmail.com>
2022-04-06 18:42:22 +0200

    Add JWT token in request headers

--- src/apis/api/axiosInstance.js
+++ src/apis/api/axiosInstance.js
@@ -4,4 +4,17 @@ const api = axios.create({
   baseURL: 'http://54.37.154.200:10000/api/v1/',
 });
 
+api.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem('JWT');
+  if (jwt) {
+    // suppress eslint rule because axios doc does not specify that config object is immutable
+    // eslint-disable-next-line no-param-reassign
+    config.headers = {
+      ...config.headers,
+      Authorization: `bearer ${jwt}`,
+    };
+  }
+  return config;
+});
+
 export default api;
