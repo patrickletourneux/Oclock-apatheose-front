646269ebdfda1cfbc7dbadbad5736ef1f7c27d17
S-Axel <sabotieraxel@gmail.com>
2022-04-06 18:12:37 +0200

    in authContext store token on login and remove it on logout  also, store base user data on login and remove it on logout make deconnexion button call logout from authContext

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -9,18 +9,28 @@ const authContext = createContext();
 
 export function AuthProvider({ children }) {
   const [authed, setAuthed] = useState(false);
+  const [user, setUser] = useState(null);
   // TODO check/decode JWT token
 
-  const login = () => {
+  const login = (authData) => {
     setAuthed(true);
+    setUser(authData.user);
+    localStorage.setItem('JWT', authData.token);
   };
 
   const logout = () => {
     setAuthed(false);
+    setUser(null);
+    localStorage.removeItem('JWT');
   };
 
   // TODO test performance difference without memo
-  const auth = useMemo(() => ({ authed, login, logout }), [authed]);
+  const auth = useMemo(() => ({
+    authed,
+    user,
+    login,
+    logout,
+  }), [authed, user]);
 
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
