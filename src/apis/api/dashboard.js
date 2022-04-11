11201ecb80b19f61641461546fe6e93305151cbf
S-Axel <sabotieraxel@gmail.com>
2022-04-11 16:01:40 +0200

    create getDashboard api call

---
+++ src/apis/api/dashboard.js
@@ -0,0 +1,59 @@
+import api from './axiosInstance';
+
+// eslint-disable-next-line no-unused-vars
+const oneHomeDataMock = {
+  home: {
+    id: 56,
+    name: 'maison des Letourneux',
+    userCount: 12,
+  },
+  reward: {
+    id: 5,
+    title: 'Massage Californien',
+    end_at: '2022-04-08 15:09:14.538557+02',
+  },
+  tasks: {
+    attributed_tasks_count: 3,
+    done_tasks_count: 5,
+  },
+  ranking: {
+    first_user: {
+      id: 45,
+      pseudonym: 'Patrick',
+      avatar_img: 'https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg',
+      score: 603,
+    },
+    current_user: {
+      id: 1,
+      pseudonym: 'Axel',
+      avatar_img: 'https://img.freepik.com/free-vector/mysterious-mafia-man-wearing-hat_52683-34829.jpg',
+      score: 512,
+      rank: 2,
+    },
+  },
+};
+
+// eslint-disable-next-line no-unused-vars
+const noHomeDataMock = {};
+
+/**
+ * @param {number} userId
+ * @param {function(object)} onSuccess
+ * @param {function(string)} onError
+ * @returns {Promise<void>}
+ *
+ */
+const getDashboardPage = async (userId, onSuccess, onError) => {
+  try {
+    const response = await api.get(`dashboard/${userId}`);
+    onSuccess(response.data);
+  } catch (error) {
+    if (error.response.status === 400 || error.response.status === 404) {
+      onError(error.response.data.message);
+    } else {
+      onError('Une erreur est survenue, veuillez réessayer plus tard');
+    }
+  }
+};
+
+export default getDashboardPage;
