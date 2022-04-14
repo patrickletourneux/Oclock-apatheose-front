c50ba627e59d938bab00d5b7f911420e22bfc74d
Webme <guillaume.gackel@gmail.com>
2022-04-14 21:19:49 +0200

    404

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,39 +1,34 @@
 import { Image } from '@mui/icons-material';
-import { Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
 import nettoyeur from '../../assets/images/nettoyeur.jpg';
 import PageContainer from '../PageContainer/PageContainer';
 
 function Page404() {
-  const styles = {
-    paperContainer: {
-		  backgroundImage: `url(${nettoyeur})`,
-		  backgroundColor: 'white',
-      // backgroundSize: '80%',
-      backgroundPosition: 'center',
-      width: '100vw',
-      margin: -24,
-      padding: 24,
-    },
-
-	  };
   return (
-    <>
-      <Typography>404 WORK IN PROGRESS</Typography>
+    <PageContainer
+      margin="30px auto"
+    >
+      <Typography
+        variant="h2"
+        textAlign="center"
+        margin="50px"
+
+      >404 WORK IN PROGRESS
+      </Typography>
 
       <Box
         component="img"
         sx={{
-          height: 233,
-          width: 350,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
+          height: 800,
+          width: 800,
+          maxHeight: { xs: 400, md: 900 },
+          maxWidth: { xs: 400, md: 900 },
         }}
         alt="nettoyeur"
         src={nettoyeur}
       />
-      <PageContainer style={styles.paperContainer} />
-    </>
+    </PageContainer>
   );
 }
 
